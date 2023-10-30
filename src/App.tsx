3b6f8c34dfe9e7a142ea3f6f599c95294f3a117a
mujabom <msaabwsr@gmail.com>
2023-10-30 16:43:10 +0300

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useEffect, useState } from "react";
 import productImage from "./asset/images/image-product-desktop.jpg";
 import productImage_m from "./asset/images/image-product-mobile.jpg";
 
 import { ShoppingCartOutlined } from "@mui/icons-material";
+import { useMediaQuery } from "@mui/material";
 
 export default function App() {
-    //TODO: implementing responsive image replacment
+  //TODO: implementing responsive image replacment
+  const isLarge = useMediaQuery("(min-width: 768px)");
+
   return (
     <div className="grid min-h-screen place-content-center bg-primary-cream">
-      <div className="m-auto flex md:w-5/12 flex-col md:flex-row rounded-2xl bg-neutral-white">
-        <img src={productImage} alt="logo" className="md:w-1/2 rounded-l-2xl" />
-        <div className="flex max-h-full  md:w-1/2 flex-col justify-between p-8 ">
-          <h4 className=" font-montserrat w-full text-lg tracking-[0.3rem] text-neutral-darkGrayishBlue">
+      <div className="m-auto flex flex-col rounded-2xl bg-neutral-white w-[21.5rem] md:w-[43rem] md:flex-row">
+        <img
+          src={isLarge ? productImage : productImage_m}
+          alt="logo"
+          className="rounded-t-2xl md:w-1/2"
+        />
+        <div className="flex max-h-full gap-4 flex-col justify-between p-8 md:w-1/2 ">
+          <h4 className=" w-full font-montserrat text-lg tracking-[0.3rem] text-neutral-darkGrayishBlue">
             PERFUME
           </h4>
           <h1 className="font-fraunces text-[2.5rem] font-bold leading-none">
@@ -22,12 +28,14 @@ export default function App() {
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
           <div className="flex gap-5">
-            <h2 className="font-fraunces text-[36px] text-primary-darkCyan font-bold leading-none">
+            <h2 className="font-fraunces text-4xl font-bold leading-none text-primary-darkCyan">
               $149.99
             </h2>
-           <p className="flex justify-center items-center text-neutral-darkGrayishBlue line-through">$169.99</p>
+            <p className="flex items-center justify-center text-neutral-darkGrayishBlue line-through">
+              $169.99
+            </p>
           </div>
-          <button className="rounded-xl hover:bg-primary-darkGreen bg-primary-darkCyan py-4 text-neutral-white">
+          <button className="rounded-xl bg-primary-darkCyan py-4 text-neutral-white hover:bg-primary-darkGreen">
             <ShoppingCartOutlined /> Add to Cart
           </button>
         </div>
